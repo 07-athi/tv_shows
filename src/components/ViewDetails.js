87d2c2f63eaf6e87755037a4d288a7097df8fa74
07-athi <athiraazad7@gmail.com>
2023-05-05 06:15:32 +0530

    new change

--- src/components/ViewDetails.js
+++ src/components/ViewDetails.js
@@ -17,12 +17,12 @@ const ViewDetails = () => {
     
   return (
     <div className='App'>
-        <div className=' p-52 pb-2 font-serif text-white text-xl '>
+        <div className=' pt-60  font-serif text-white '>
         <h3>Name: {shows?.name}</h3>
         </div>
-        <div className=' pb-2 font-serif text-white text-2xl '><h3>Genre: { shows?.type}</h3></div>
-        <div className=' pb-2 font-serif text-white text-2xl '><h3>Language: {shows?.language}</h3></div>
-        <div className=' flex justify-center items-centerpb-2 font-serif text-white text-2xl p-40 '><h3>Summary: {shows.summary}</h3></div>
+        <div className=' pb-2 font-serif text-white '><h3>Genre: { shows?.type}</h3></div>
+        <div className=' pb-2 font-serif text-white  '><h3>Language: {shows?.language}</h3></div>
+        <div className=' flex justify-center items-centerpb-2 font-serif text-white  pt-7 md:p-52 p-28 '><h3>Summary: {shows.summary}</h3></div>
         
         
     </div>
